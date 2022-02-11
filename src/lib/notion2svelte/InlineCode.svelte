734533de1c685db1824ea6acf44aaca79e16b8ad
Elbong Gearny <elbong@navelgazer.club>
2022-02-11 15:21:02 -0800

    `npx degit nvlgzr/n2s-starter-components/notion2svelte src/lib/notion2svelte`
    (Plus, pinned Svelte Kit to next.259 because 260 introduces a "shadow enpoint" feature that requires a [workaround](https://github.com/sveltejs/kit/issues/3759), and I'd just as soon wait till the fix is in to unpin the kit.)

---
+++ src/lib/notion2svelte/InlineCode.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let code = '';
+</script>
+
+<span><code>{@html code}</code></span>
+
+<style>
+  span {
+    font-family: 'SFMono-Regular', Menlo, Consolas, 'PT Mono', 'Liberation Mono', Courier, monospace;
+    background: hsla(0, 0%, 100%, 0.45);
+    color: #eb5757;
+    border-radius: 3px;
+    font-size: 85%; /* Sets height of gray box */
+    padding: 0.1rem 0.2rem 0.1rem 0.2rem;
+  }
+
+  code {
+    display: inline;
+    background: hsla(0, 80%, 95%);
+    color: inherit;
+    border-radius: 3px;
+    font-size: 85%;
+    white-space: pre-wrap;
+    padding: 0.2rem 0.5rem 0.2rem 0.5rem;
+  }
+</style>
---
+++ src/lib/notion2svelte/Paragraph.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let blockProps = {};
+</script>
+
+<p>
+  {#if $$slots.default}
+    <div title={JSON.stringify(blockProps, null, 2)}><slot /></div>
+  {:else}
+    &nbsp; <!-- Ensures empty blocks get rendered -->
+  {/if}
+</p>
+
+<style>
+  p {
+    padding: 0 0 0.5rem 0;
+    margin: 0;
+    position: relative;
+  }
+</style>
