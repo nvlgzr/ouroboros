51f84b29a429337c0498be3d1486ed25bc82a0df
Elbong Gearny <elbong@navelgazer.club>
2022-03-10 14:48:24 -0800

    Hi, I made you a thing!
    A page for Mr. T.

---
+++ src/routes/hiimadeyouathing/index.svelte
@@ -0,0 +1,39 @@
+<div>
+	<img
+		class="isitart"
+		src="/itisart.png"
+		alt={`On the wall in the back of a restaurant in Bodega Bay, partly covered by a stack of boxes, hangs a white-on-black illustration of Lionel Richie's head accompanied by the words "Is it me—". This image crops the photograph I took of that poster so that only the "Is it" remains, and adds "art?" to the end. So, to summarize, this is a line-art rendition of Lionel Richie next to the phrase, "Is it art?"`}
+	/>
+	<p>Hi. I made you a thing.</p>
+	<a href="/yes.png"
+		><img
+			src="/yes.png"
+			alt={`Trying to describe this collage in short order is a fool's errand. Rather than describing what is shown, I will tell you how I made it. But first, some background. I spent a summer during college with some high school friends in a rented house above the Santa Cruz Boardwalk. Among our many adventures was a telephone encounter between…let's call him, Mr. T, and one of the two crabby old landladies, recorded by accident by…call him H. H gathered all the housemates together upon discovering the recording, and we all had a great laugh at Mr. T's yawning, gob-smacking sounds of confused half-asleepedness on the one hand, and the landlady's weak attempts at empathy on the other. The conversation had been a farce, and, buried within that farce, was a single exchange which I later captured for posterity as part of a remix built on top of a mediocre drum loop and a moody bassline recorded by H. In the recording, L says to Mr. T, "It looked like…one of you was…not an artist, or something, but, you know, if—" Mr. T then cuts her off, saying, "Yes, I am an artist, actually." That's backstory part 1. Backstory part 2 takes us back to Bodega Bay, where I recently spent some time with Mr. T again. We ended up discussing wombo.art, a free tool for generating images from text prompts. Mr. T poo-pooed the images, poo-pooed the software's makers, and said some poopy things about software businesses as a whole. I have plenty of criticisms and concerns attached to concepts like Amazon®, Facebook®, Google®, but I strive to include ad hominems in my thinking. I also try to take something useful out of everything…even the things that scare me. Wombo.art scares me. It is a precursor to a world bent on swallowing me in its ever-accellerating dance with chaos. You see, as a programmer, I understand exponential effects. I take the threats of AI seriously. But as a quasi-Buddhist, I lean toward acceptance. With all of this in mind, here is what I did to make this image. First, I fed wombo.art the phrase, "Yes, I am an artist, actually" to every one of its built-in "flavors." I then selected 17 of the most, for lack of a better word, "compelling" results (some flavors just had nothing useful to contribute!), and croppy-pasted them into Figma, randomly placed to roughly fill a circular space, with the words, "But…is it art?" somewhere near the center. (That phrase has yet another backstory, which I won't go into here, except to say that the actual phrase from the story is "Is it art, or is it shit?".). Over each image, I overlaid the seed phrase, "Yes, I am an artist, actually." Each version of the phrase uses a different font, with different colors, effects and ornamentation applied as the mood suited me, taking my inspiration from…of course…the AI generated images. What's the point? Well, mainly just so I can share this web page with Mr. T as a form of intellectual trolling. But also, I find those two phrases so interesting. "Yes, I am an artist, actually," and "Is it art?" What makes art? What makes an artist? How much unpaid creation do I need to flounder through before I go from "just some guy" to "a real artist"? Am I still just a wooden toy with a strangely-flexible nose? Or is this enough? Is this page enough? Am I an artist? Is this art?`}
+		/></a
+	>
+	<p>also → <a href="https://castro.fm/episode/9Cgyhr">this</a></p>
+</div>
+
+<style>
+	:root,
+	a {
+		background-color: black;
+		color: hsl(29, 67%, 35%);
+	}
+	div {
+		display: grid;
+		align-content: center;
+		justify-items: center;
+	}
+	img {
+		max-width: 80vw;
+	}
+	p {
+		font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+		font-size: 3vh;
+		padding: 2rem 1rem;
+	}
+	.isitart {
+		max-width: 30vw;
+	}
+</style>
