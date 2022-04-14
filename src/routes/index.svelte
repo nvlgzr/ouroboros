3b70031813bb5e17cfd4ce8e984b618a21836094
Elbong Gearny <elbong@navelgazer.club>
2022-04-14 13:57:25 -0700

    Added link from intro animation to TNT (& tweaked the alt text)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,10 +3,12 @@
 	<meta name="author" content="Elbong Gearny" />
 </svlete:head>
 
-<img
-	src="/navelgazer-animated.svg"
-	alt="An animated introduction to the navelgazer logo, in which a sort of bald-headed alien face, seen from the checkbones up, suddenly sprouts a third eye on its ginormous forehead, then, after a bit of looking around, the entire outline of the head flops forward, hinged at the base of those 'cheekbones', flipping to reveal itself as not an alien head, but a wide belly. The 'third eye' is now below the other two eye dots, and what had looked like brow ridges now look like man-boobs with two used-to-be-eye nipples and a used-to-be-third-eye belly button. After a pause, the belly button 'looks' languidly left, then right, then snaps back, almost guiltily, to center."
-/>
+<a href="/theStoryteller/Book/0/Prolog/the-north-tower">
+  <img
+    src="/navelgazer-animated.svg"
+    alt="An animated introduction to the navelgazer logo, in which a noseless, lineart sort of…bald-headed alien face—seen from the checkbones up—suddenly sprouts a third eye on its ginormous forehead, then, after a bit of looking around, the entire outline of the head flops forward, hinged at the base of those 'cheekbones', flipping to reveal itself as not an alien head, but a wide belly. The 'third eye' is now below the other two eye dots, and what had looked like brow ridges now look like man-boobs with two used-to-be-eye nipples and a used-to-be-third-eye belly button. After a pause, the belly button 'looks' languidly left, then right, then snaps back, almost guiltily, to center."
+  />
+</a>
 
 <style>
 	:root {
