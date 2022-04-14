fa8164a8ae6782b4c2c542866dc276bcd7dc8d5c
Elbong Gearny <elbong@navelgazer.club>
2022-04-14 00:39:52 -0700

    Fix missing cover art by providing a target `<div id="cover"…` in the default layout
    (I suspect this will move to either a named layout or down a level into a storyteller-specific layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,41 @@
+<script>
+</script>
+
+<div id="cover" style="height:0">&nbsp;</div>
+<main>
+  <slot />
+</main>
+
+<footer>
+  <p>
+    <a href="/">Front of the Club</a>
+  </p>
+</footer>
+
+<style>
+  :root {
+    --main-margin: 4rem;
+    --footer-gray: hsl(224, 9%, 50%);
+    --white: hsl(0, 4%, 95%);
+    --font: ;
+    --reading-width: 35rem;
+  }
+  main {
+    margin: var(--main-margin);
+    max-width: var(--reading-width);
+  }
+  footer * {
+    padding: 1rem;
+    margin: 0 auto;
+    color: var(--white);
+    text-decoration: none;
+    display: flex;
+  }
+  footer p {
+    background-color: var(--footer-gray);
+  }
+  p {
+    margin: 0;
+    background-color: aliceblue;
+  }
+</style>
