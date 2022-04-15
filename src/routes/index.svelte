44eb94a1563347d6e12e49795f35be083a3329af
Elbong Gearny <elbong@navelgazer.club>
2022-04-15 01:09:57 -0700

    Fixed issue where navigating from top index to the-north-tower resulted in fubar'd layout
    Turns out I hadn't quite grokked the way :root can act like an "invisible" layout. I suspect I'll try to avoid using :root in general, as I like my components to generally behave idempotently, regardless of what page came before. This behavior's frankly kinda odd. ¯\_(ツ)_/¯

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,10 @@
+<script>
+  import IntroAnimation from './intro.svelte';
+</script>
+
 <svlete:head>
   <title>The Procrastinator's Guide to…</title>
   <meta name="author" content="Elbong Gearny" />
 </svlete:head>
 
-<a href="/theStoryteller/Book/0/Prolog/the-north-tower">
-  <img
-    src="/navelgazer-animated.svg"
-    alt="An animated introduction to the navelgazer logo, in which a noseless, lineart sort of…bald-headed alien face—seen from the checkbones up—suddenly sprouts a third eye on its ginormous forehead, then, after a bit of looking around, the entire outline of the head flops forward, hinged at the base of those 'cheekbones', flipping to reveal itself as not an alien head, but a wide belly. The 'third eye' is now below the other two eye dots, and what had looked like brow ridges now look like man-boobs with two used-to-be-eye nipples and a used-to-be-third-eye belly button. After a pause, the belly button 'looks' languidly left, then right, then snaps back, almost guiltily, to center."
-  />
-</a>
-
-<style>
-  :root {
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  img {
-    max-width: 50vh;
-    min-width: 20rem;
-    position: relative;
-    top: -10rem;
-  }
-</style>
+<IntroAnimation />
