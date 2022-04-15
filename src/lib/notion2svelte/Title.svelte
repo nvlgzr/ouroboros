4e6b3202fa2ba921b199eedc866ba0d203b31f62
Elbong Gearny <elbong@navelgazer.club>
2022-04-14 23:33:55 -0700

    Hacked-in Noto fonts
    There's obviously a better way to do the svelte:head stuff…and I've got plans to incorporate Normalize and…some sort of pure CSS, utility-first system. For now, I just need things looking good enough to get the story pushed off my plate.

--- src/lib/notion2svelte/Title.svelte
+++ src/lib/notion2svelte/Title.svelte
@@ -1,12 +1,20 @@
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display:ital,wght@0,100;0,300;0,500;0,700;0,900;1,100;1,300;1,500;1,700;1,900&family=Noto+Serif:ital@0;1&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
 <span><slot /></span>
 
 <style>
   span {
-    font-weight: 700;
+    font-weight: 300;
     line-height: 1.2;
     font-size: 3.2rem;
-    font-family: ui-sans-serif, -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica,
-      'Apple Color Emoji', Arial, sans-serif, 'Segoe UI Emoji', 'Segoe UI Symbol';
+    font-family: 'Noto Serif Display', serif;
     cursor: text;
     display: flex;
     align-items: center;
--- src/routes/theStoryteller/__layout.svelte
+++ src/routes/theStoryteller/__layout.svelte
@@ -1,5 +1,11 @@
-<script>
-</script>
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display:ital,wght@0,100;0,300;0,500;0,700;0,900;1,100;1,300;1,500;1,700;1,900&family=Noto+Serif:ital@0;1&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
 
 <div id="cover" style="height:0">&nbsp;</div>
 <main>
@@ -17,15 +23,24 @@
     --main-margin: 4rem;
     --footer-gray: hsl(224, 9%, 50%);
     --white: hsl(0, 4%, 95%);
-    --font: ;
+    --body-text: 'Noto Serif', serif;
+    --body-spacing: 1.6rem;
+    --body-weight: 400;
+    --display-font: 100 1.6rem/1.6rem 'Noto Serif Display', serif / 3.2rem;
     --reading-width: 35rem;
   }
   main {
     margin: var(--main-margin);
     max-width: var(--reading-width);
+    font-family: var(--body-text);
+    font-weight: var(--body-weight);
+    line-height: var(--body-spacing);
+  }
+  footer {
+    font: 100 1.6rem 'Noto Serif Display', serif;
   }
   footer * {
-    padding: 1rem;
+    padding: 0.95rem 0 1rem 0;
     margin: 0 auto;
     color: var(--white);
     text-decoration: none;
