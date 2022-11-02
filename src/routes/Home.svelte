eb940ec9230411fb6871915d69972e330e39dc74
Elbong Gearny <elbong@navelgazer.club>
2022-11-02 01:23:12 -0700

    Added TheStoryteller cover

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -7,7 +7,13 @@
 <main>
   <Nav />
   <section>
-    <Book title="TheStoryteller()" url="/TheStoryteller/Book/0/Prolog/the-north-tower" />
+    <Book title="TheStoryteller()" url="/TheStoryteller/Book/0/Prolog/the-north-tower">
+      <img
+        style="width: 11.3rem; filter: drop-shadow(0.4rem 0.4rem 0.9rem hsl(0, 0%, 0%, 50%));"
+        alt="Cover Art displaying the title—Who Killed Adrim Wiren?—over a sunset-tinged fog into which is falling a shadowy figure in red high heels. Only the heels and the hands remain above the pink-and-purple cloud layer, and each hand is offering up a middle finger. Oh, and there's like, a robot emoji mask flying away, and a sort of paper butterfly casting a long shadow, and rust-colored suspension bridge cables, and, well…the rest makes for a boring description."
+        src="/the-storyteller-cover_832x1112.png"
+      />
+    </Book>
     <Book title="Still Life" url="/StillLife">
       <StillLifeCover />
     </Book>
--- src/routes/StillLifeCover.svelte
+++ src/routes/StillLifeCover.svelte
@@ -1,10 +1,5 @@
 <picture>
   <div>
-    <!-- <img
-      class="vector-overlay"
-      src="/life-alone.png"
-      alt="The words, 'Still Life' 'with the 'Still' in fragmented letters which vibrate quietly, and 'Life' in a vibrant collage: full of life, but completely inert."
-    /> -->
     <img class="raster-underlay" src="/still-life-cover_832x1112.png" alt="Life" />
     <h1>Still</h1>
   </div>
@@ -23,13 +18,6 @@
     top: -2rem;
     font-family: Chalkduster;
     font-size: 3.7rem;
-  }
-  picture {
-  }
-  img {
-    height: 30vh;
-  }
-  .vector-overlay {
   }
   .raster-underlay {
     filter: drop-shadow(0.4rem 0.4rem 0.9rem hsl(0, 0%, 0%, 30%));
