0fdb78f2902ad05180780e63c46c42bcf8de8e2e
Elbong Gearny <elbong@navelgazer.club>
2022-11-03 12:58:38 -0700

    Consolidated Google Font requests into single <link>

--- src/lib/notion2svelte/Title.svelte
+++ src/lib/notion2svelte/Title.svelte
@@ -1,12 +1,3 @@
-<svelte:head>
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display:ital,wght@0,100;0,300;0,500;0,700;0,900;1,100;1,300;1,500;1,700;1,900&family=Noto+Serif:ital@0;1&display=swap"
-    rel="stylesheet"
-  />
-</svelte:head>
-
 <span><slot /></span>
 
 <style>
--- src/routes/TheStoryteller/+layout.svelte
+++ src/routes/TheStoryteller/+layout.svelte
@@ -1,13 +1,5 @@
-<svelte:head>
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display:ital,wght@0,100;0,300;0,500;0,700;0,900;1,100;1,300;1,500;1,700;1,900&family=Noto+Serif:ital@0;1&display=swap"
-    rel="stylesheet"
-  />
-</svelte:head>
-
 <div id="cover" style="height:0">&nbsp;</div>
+
 <main>
   <section>
     <div class="left">&nbsp;</div>
