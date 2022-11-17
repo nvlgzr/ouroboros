82e2fd326e4488adf23725bd0afe570b9234e45e
Elbong Gearny <elbong@navelgazer.club>
2022-11-16 20:17:54 -0800

    Fixed StillLife stacking so Footer is at bottom*
    *Albeit still all wonky!

--- src/routes/StillLife/+layout.svelte
+++ src/routes/StillLife/+layout.svelte
@@ -8,7 +8,9 @@
       <slot />
     </vstack>
   </section>
-  <Footer />
+  <section>
+    <Footer />
+  </section>
 </main>
 
 <style>
@@ -18,11 +20,8 @@
     color: hsl(169, 15%, 14%);
     background: radial-gradient(50% 133.13% at 50% 50%, white 0% 20%, hsl(17, 20%, 93%) 100%);
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-  }
-  vstack {
-    max-width: min(90%, 31rem);
-    /* background: green; */
   }
 </style>
--- src/routes/StillLife/Footer.svelte
+++ src/routes/StillLife/Footer.svelte
@@ -4,49 +4,22 @@
   import SiteLink from '$lib/SiteLink.svelte';
 </script>
 
-<vstack>
-  <hstack>
-    <StillLife />
-    <Copyright />
-    <SiteLink />
-  </hstack>
-</vstack>
+<hstack align-y="bottom">
+  <StillLife horizontal />
+  <spacer />
+  <Copyright />
+  <spacer />
+  <SiteLink />
+</hstack>
 
 <style>
-  @media (hover: none) {
-    a {
-      background-color: hsla(169, 15%, 94%, 0.5);
-      padding: min(0.25rem, 0.5vw) 0.5rem;
-      border-radius: 0.25rem;
-    }
-  }
-  vstack {
+  hstack {
     position: relative;
     margin-top: 4rem;
     font: 100 1.6rem 'Noto Serif', serif;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
+    max-width: min(80rem, 100vw);
   }
-  img {
-    width: 100vw;
-  }
-  hstack {
-    position: absolute;
-    width: 100vw;
-    bottom: 0rem;
-    z-index: 1;
-    height: fit-content;
-    color: var(--paper);
-    align-items: baseline;
-  }
-  a {
-    color: var(--paper);
-    margin: min(1rem, 4.5vw) min(2rem, 1vw);
-    text-decoration: none;
-    font-family: 'Noto Serif Display';
-    font-size: min(3rem, 3.5vw);
-  }
-  a:hover {
-    filter: brightness(55%);
+  spacer {
+    min-width: 5vw;
   }
 </style>
