71ffa2dadc15aa1a34dbfc7ee2b56f09e14ffdf2
Elbong Gearny <elbong@navelgazer.club>
2022-11-17 20:43:28 -0800

    Extract Component: Footer → TheStoryteller

--- src/routes/TheStoryteller/Footer.svelte
+++ src/routes/TheStoryteller/Footer.svelte
@@ -1,4 +1,5 @@
 <script>
+  import TheStoryteller from './TheStoryteller.svelte';
   import Copyright from '$lib/Copyright.svelte';
   import SiteLink from '$lib/SiteLink.svelte';
 </script>
@@ -6,20 +7,13 @@
 <vstack>
   <img src="/the-storyteller-footer.svg" alt="" />
   <hstack>
-    <a href="/TheStoryteller">TheStoryteller()</a>
+    <TheStoryteller />
     <Copyright />
     <SiteLink />
   </hstack>
 </vstack>
 
 <style>
-  @media (hover: none) {
-    a {
-      background-color: hsla(169, 15%, 94%, 0.5);
-      padding: min(0.25rem, 0.5vw) 0.5rem;
-      border-radius: 0.25rem;
-    }
-  }
   vstack {
     position: relative;
     margin-top: 4rem;
@@ -38,15 +32,5 @@
     height: fit-content;
     color: var(--paper);
     align-items: baseline;
-  }
-  a {
-    color: var(--paper);
-    margin: min(1rem, 4.5vw) min(2rem, 1vw);
-    text-decoration: none;
-    font-family: 'Noto Serif Display';
-    font-size: min(3rem, 3.5vw);
-  }
-  a:hover {
-    filter: brightness(55%);
   }
 </style>
