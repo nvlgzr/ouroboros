fa2790128fcc1b2112cef9b34ea2270c22e60e0a
Elbong Gearny <elbong@navelgazer.club>
2022-12-09 02:37:33 -0800

    Added Underline component to match notion2svelte's new import

---
+++ src/lib/StillLife/Underline.svelte
@@ -0,0 +1,13 @@
+<span><slot /></span>
+
+<style>
+  span {
+    /*
+       Red because I don't expect to use
+       underlines so this will make sure I
+       notice if one happens to sneak through
+    */
+    text-decoration: red wavy underline;
+  }
+</style>
+
