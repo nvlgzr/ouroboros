a6d393e6170526388133806e82690d61ef5d479f
Elbong Gearny <elbong@navelgazer.club>
2022-12-13 12:53:28 -0800

    Updated StillLife Table of Contents w/dots between page & title spans
    🙄 I've considered automating this, but it's unclear that doing so would afford much of an advantage, esp. since I find it helpful to be slowed down from time to time in order to truly spend time with the content.

--- src/routes/StillLife/+page.svelte
+++ src/routes/StillLife/+page.svelte
@@ -1,11 +1,24 @@
 <script>
+  import { page } from '$app/stores';
   import StillLife from '../StillLife.svelte';
+
+  const pages = [
+    { path: '', title: 'Table of Contents' },
+    { path: 'bookends', title: 'Bookends' },
+    { path: 'forgiveness', title: 'Forgiveness' },
+    { path: 'malespero', title: 'malespero' },
+    { path: 'bellydown', title: 'Untitled' },
+    { path: 'vessel', title: 'Vessel' },
+    { path: 'folkbytheoakfedmepokeandscoke', title: 'Crone' },
+    { path: 'lastthingsfirst', title: '⇌' },
+    { path: 'beached', title: 'Whale' }
+  ];
 </script>
 
-<a href="/">
+<a class="skuller" href="/">
   <img
     src="/skulking-skuller.svg"
-    alt="In the corner of the 'page', the skuller lurks, pearing at you over a fold in the 'paper'."
+    alt="In the corner of the 'page', the skuller lurks, peering at you over a fold in the 'paper'. Follow it to close this book and return to tiny club library."
   />
 </a>
 
@@ -15,41 +28,75 @@
     <StillLife />
   </div>
   <spacer />
-  <h1><a href="/StillLife/bookends">page 1</a></h1>
-  <spacer />
-  <h1><a href="/StillLife/forgiveness">page 2</a></h1>
-  <spacer />
-  <h1><a href="/StillLife/malespero">page 3</a></h1>
-  <spacer />
-  <h1><a href="/StillLife/" class="disabled">page 4</a></h1>
-  <spacer />
-  <h1><a href="/StillLife/" class="disabled">page 5</a></h1>
-  <spacer />
-  <h1><a href="/StillLife/" class="disabled">page 6</a></h1>
-  <spacer />
-  <h1><a href="/StillLife/" class="disabled">page 7</a></h1>
+  <ul>
+    {#each pages as page, index}
+      <li>
+        <a class="pagelink" href="/StillLife/{page.path}">
+          <span class="index">p. {index}</span><span class="chapter">{page.title}</span>
+        </a>
+      </li>
+      <spacer />
+    {/each}
+    <li>more to come</li>
+  </ul>
   <spacer />
 </vstack>
 
 <style>
-  h1 {
-    pointer-events: none;
+  li {
     transform: skew(5deg, 5deg) !important;
+    padding: 0.75rem;
+    display: flex;
   }
   a {
-    pointer-events: auto;
     text-decoration: none;
     color: hsl(169, 15%, 14%);
+    display: flex;
+    width: min(20rem, 80vw);
   }
-  h1:hover,
+  a:after {
+    background-image: radial-gradient(circle, currentcolor 0.25px, transparent 1px);
+    background-position: bottom;
+    background-size: 1.25ex 0.1rem;
+    background-repeat: space no-repeat;
+    margin: 0 0.25rem;
+    content: '';
+    flex-grow: 1;
+    height: 1rem;
+  }
+  a.skuller:after {
+    /* Prevent dots from rendering to the right of Skuller image */
+    content: none;
+  }
+  li:hover,
   a:hover {
     color: hsl(169, 25%, 44%);
   }
-  .disabled {
-    color: hsl(169, 25%, 84%);
+  a:hover span:first-of-type {
+    color: black;
   }
+  li .chapter {
+    order: 3;
+  }
+  li:first-of-type,
+  li:last-of-type {
+    pointer-events: none;
+  }
+  li:last-of-type {
+    color: hsl(169, 25%, 54%);
+  }
+  li:last-of-type::after {
+    background-image: radial-gradient(circle, currentcolor 0.25px, transparent 2px);
+    background-position: bottom;
+    background-size: 0.8ex 0.1rem;
+    background-repeat: space no-repeat;
+    content: '';
+    flex-grow: 0.1;
+    height: 1rem;
+  }
+
   @media (hover: none) {
-    h1,
+    li,
     a {
       background-color: hsl(169, 25%, 93%);
       padding: min(0.25rem, 0.5vw) 0.5rem;
