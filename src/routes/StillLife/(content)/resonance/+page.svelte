fcc47777ebcc0dab20bf9ee7a814b82e144db628
Elbong Gearny <elbong@navelgazer.club>
2023-01-03 03:30:44 -0800

    Content: resonance

---
+++ src/routes/StillLife/(content)/resonance/+page.svelte
@@ -0,0 +1,106 @@
+<script>
+  import Error from '$lib/StillLife/Error.svelte';
+  import Cover from '$lib/StillLife/Cover.svelte';
+  import Title from '$lib/StillLife/Title.svelte';
+  import BulletedListItem from '$lib/StillLife/BulletedListItem.svelte';
+  import Callout from '$lib/StillLife/Callout.svelte';
+  import ChildPage from '$lib/StillLife/ChildPage.svelte';
+  import Code from '$lib/StillLife/Code.svelte';
+  import ColumnList from '$lib/StillLife/ColumnList.svelte';
+  import Column from '$lib/StillLife/Column.svelte';
+  import Divider from '$lib/StillLife/Divider.svelte';
+  import Embed from '$lib/StillLife/Embed.svelte';
+  import Header from '$lib/StillLife/Header.svelte';
+  import Image from '$lib/StillLife/Image.svelte';
+  import IndentGroup from '$lib/StillLife/IndentGroup.svelte';
+  import Underline from '$lib/StillLife/Underline.svelte';
+  import InlineCode from '$lib/StillLife/InlineCode.svelte';
+  import InlineColor from '$lib/StillLife/InlineColor.svelte';
+  import Equation from '$lib/StillLife/Equation.svelte';
+  import Link from '$lib/StillLife/Link.svelte';
+  import NumberedListItem from '$lib/StillLife/NumberedListItem.svelte';
+  import Paragraph from '$lib/StillLife/Paragraph.svelte';
+  import Quote from '$lib/StillLife/Quote.svelte';
+  import TodoItem from '$lib/StillLife/TodoItem.svelte';
+  import Toggle from '$lib/StillLife/Toggle.svelte';
+  import { setContext } from 'svelte';
+
+  setContext('pageStuff', {
+    pageId: 'ae0a073f19fc4fcc9aca55e694343a65',
+    title: 'Resonance',
+    previous: '',
+    next: ''
+  });
+</script>
+
+<svelte:head>
+  <title>Resonance</title>
+</svelte:head>
+
+<Cover coverURL={''} iconEmojiOrURL={'undefined'} />
+
+<Title>Resonance</Title>
+
+<Code
+  code={`
+You!
+You ring within me, singing
+	&quot;Life! Life!&quot; to the tips of my toes
+	each cell re-sounding the call
+	adding its tiny voice to the millions
+	drops of water in the wave
+	liliputians lifting me toward the sky
+	at just the thought of you
+
+You!
+We re-call your breathless vibrations
+	my cells and I
+	your unrationed passions
+	the free flow of your dance
+	that loving invitation to breach
+	to sink into your undulating curves
+	I slide across your back
+	no risk of rushing my affections
+	I am on board
+	at your speed
+
+You!
+You let the flood of me fill your lungs breathless
+	your tumbling heart pounds in my good ear
+	a song of you rising and falling in languid crescendos
+		rocking me gently
+		swallowing me in wet curls
+		squeezing me from every angle
+		tossing me in slow motion
+		elevating my body my mind
+		I am pounded on your shore
+		sinking into the sun-warmed sand of your smile
+		(ahhhh, and your surprising splash!)
+
+You!!!
+I rise to your toll too early, yet
+	drowning in gratitude
+At night we race unicorns until it&#039;s too late, and
+	#sorrynotsorry #nothuman #noregrets
+
+You…
+Recede to The Source
+	undulating in other worlds
+	I dive into your every crest
+	and we rise rise rise higher
+	salt in our crevices
+	frothing at the lips
+
+I?
+I ask for a sine
+	a measure of the mystery
+	your crashing whispers
+	require no answer
+	letting the question
+	answer itself
+
+Are you my resident frequency?
+
+■`}
+  language="plain text"
+/>
---
+++ src/routes/StillLife/(content)/resonance/notion-export.js
@@ -0,0 +1,261 @@
+const json = {
+  "object": "page",
+  "id": "ae0a073f-19fc-4fcc-9aca-55e694343a65",
+  "created_time": "2023-01-03T10:37:00.000Z",
+  "last_edited_time": "2023-01-03T11:29:00.000Z",
+  "created_by": {
+    "object": "user",
+    "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+  },
+  "last_edited_by": {
+    "object": "user",
+    "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+  },
+  "cover": null,
+  "icon": null,
+  "parent": {
+    "type": "database_id",
+    "database_id": "64ff98d9-61c5-4d93-ad3e-b49400276cda"
+  },
+  "archived": false,
+  "properties": {
+    "prevPage": {
+      "id": "BHqW",
+      "type": "rollup",
+      "rollup": {
+        "type": "array",
+        "array": [],
+        "function": "show_original"
+      }
+    },
+    "Public URL": {
+      "id": "CBit",
+      "type": "formula",
+      "formula": {
+        "type": "string",
+        "string": "https://navelgazer.club/StillLife/resonance"
+      }
+    },
+    "§previous": {
+      "id": "C%7DnU",
+      "type": "relation",
+      "relation": [],
+      "has_more": false
+    },
+    "nextPage": {
+      "id": "D%5CYc",
+      "type": "rollup",
+      "rollup": {
+        "type": "array",
+        "array": [],
+        "function": "show_original"
+      }
+    },
+    "Status": {
+      "id": "D%60yI",
+      "type": "select",
+      "select": {
+        "id": "zG{S",
+        "name": "Publishable",
+        "color": "purple"
+      }
+    },
+    "Successful Read-throughs": {
+      "id": "EB%7De",
+      "type": "select",
+      "select": null
+    },
+    "Path": {
+      "id": "E_k%5E",
+      "type": "rich_text",
+      "rich_text": []
+    },
+    "§next": {
+      "id": "QhUz",
+      "type": "relation",
+      "relation": [],
+      "has_more": false
+    },
+    "Next Draft": {
+      "id": "a%3BJF",
+      "type": "relation",
+      "relation": [],
+      "has_more": false
+    },
+    "Slug": {
+      "id": "erwH",
+      "type": "formula",
+      "formula": {
+        "type": "string",
+        "string": "/resonance"
+      }
+    },
+    "§n2sLib": {
+      "id": "gWCh",
+      "type": "rich_text",
+      "rich_text": [
+        {
+          "type": "text",
+          "text": {
+            "content": "$lib/StillLife",
+            "link": null
+          },
+          "annotations": {
+            "bold": false,
+            "italic": false,
+            "strikethrough": false,
+            "underline": false,
+            "code": false,
+            "color": "default"
+          },
+          "plain_text": "$lib/StillLife",
+          "href": null
+        }
+      ]
+    },
+    "Last edited": {
+      "id": "hs%5CU",
+      "type": "last_edited_time",
+      "last_edited_time": "2023-01-03T11:29:00.000Z"
+    },
+    "Created": {
+      "id": "q%3A%7Dm",
+      "type": "created_time",
+      "created_time": "2023-01-03T10:37:00.000Z"
+    },
+    "Previous Draft": {
+      "id": "qdTh",
+      "type": "relation",
+      "relation": [
+        {
+          "id": "50713fba-8c26-48a9-bb42-66cd4ec12975"
+        }
+      ],
+      "has_more": false
+    },
+    "URL": {
+      "id": "r%3Ajk",
+      "type": "url",
+      "url": null
+    },
+    "": {
+      "id": "vDZZ",
+      "type": "created_time",
+      "created_time": "2023-01-03T10:37:00.000Z"
+    },
+    "Mine": {
+      "id": "w%3Dug",
+      "type": "checkbox",
+      "checkbox": true
+    },
+    "Local URL": {
+      "id": "yecv",
+      "type": "formula",
+      "formula": {
+        "type": "string",
+        "string": "http://localhost:5173/StillLife/resonance"
+      }
+    },
+    "Page": {
+      "id": "%7BZlR",
+      "type": "rich_text",
+      "rich_text": [
+        {
+          "type": "text",
+          "text": {
+            "content": "resonance",
+            "link": null
+          },
+          "annotations": {
+            "bold": false,
+            "italic": false,
+            "strikethrough": false,
+            "underline": false,
+            "code": false,
+            "color": "default"
+          },
+          "plain_text": "resonance",
+          "href": null
+        }
+      ]
+    },
+    "Name": {
+      "id": "title",
+      "type": "title",
+      "title": [
+        {
+          "type": "text",
+          "text": {
+            "content": "Resonance",
+            "link": null
+          },
+          "annotations": {
+            "bold": false,
+            "italic": false,
+            "strikethrough": false,
+            "underline": false,
+            "code": false,
+            "color": "default"
+          },
+          "plain_text": "Resonance",
+          "href": null
+        }
+      ]
+    }
+  },
+  "url": "https://www.notion.so/Resonance-ae0a073f19fc4fcc9aca55e694343a65",
+  "blocks": [
+    {
+      "object": "block",
+      "id": "ca2b42d0-7146-403a-a433-ae4f7261dd68",
+      "parent": {
+        "type": "page_id",
+        "page_id": "ae0a073f-19fc-4fcc-9aca-55e694343a65"
+      },
+      "created_time": "2023-01-03T10:37:00.000Z",
+      "last_edited_time": "2023-01-03T11:29:00.000Z",
+      "created_by": {
+        "object": "user",
+        "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+      },
+      "last_edited_by": {
+        "object": "user",
+        "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+      },
+      "has_children": false,
+      "archived": false,
+      "type": "code",
+      "code": {
+        "caption": [],
+        "language": "plain text",
+        "text": [
+          {
+            "type": "text",
+            "text": {
+              "content": "\nYou!\nYou ring within me, singing\n\t\"Life! Life!\" to the tips of my toes\n\teach cell re-sounding the call\n\tadding its tiny voice to the millions\n\tdrops of water in the wave\n\tliliputians lifting me toward the sky\n\tat just the thought of you\n\nYou!\nWe re-call your breathless vibrations\n\tmy cells and I\n\tyour unrationed passions\n\tthe free flow of your dance\n\tthat loving invitation to breach\n\tto sink into your undulating curves\n\tI slide across your back\n\tno risk of rushing my affections\n\tI am on board\n\tat your speed\n\nYou!\nYou let the flood of me fill your lungs breathless\n\tyour tumbling heart pounds in my good ear\n\ta song of you rising and falling in languid crescendos\n\t\trocking me gently\n\t\tswallowing me in wet curls\n\t\tsqueezing me from every angle\n\t\ttossing me in slow motion\n\t\televating my body my mind\n\t\tI am pounded on your shore\n\t\tsinking into the sun-warmed sand of your smile\n\t\t(ahhhh, and your surprising splash!)\n\nYou!!!\nI rise to your toll too early, yet\n\tdrowning in gratitude\nAt night we race unicorns until it's too late, and\n\t#sorrynotsorry #nothuman #noregrets\n\nYou…\nRecede to The Source\n\tundulating in other worlds\n\tI dive into your every crest\n\tand we rise rise rise higher\n\tsalt in our crevices\n\tfrothing at the lips\n\nI?\nI ask for a sine\n\ta measure of the mystery\n\tyour crashing whispers\n\trequire no answer\n\tletting the question\n\tanswer itself\n\nAre you my resident frequency?\n\n■",
+              "link": null
+            },
+            "annotations": {
+              "bold": false,
+              "italic": false,
+              "strikethrough": false,
+              "underline": false,
+              "code": false,
+              "color": "default"
+            },
+            "plain_text": "\nYou!\nYou ring within me, singing\n\t\"Life! Life!\" to the tips of my toes\n\teach cell re-sounding the call\n\tadding its tiny voice to the millions\n\tdrops of water in the wave\n\tliliputians lifting me toward the sky\n\tat just the thought of you\n\nYou!\nWe re-call your breathless vibrations\n\tmy cells and I\n\tyour unrationed passions\n\tthe free flow of your dance\n\tthat loving invitation to breach\n\tto sink into your undulating curves\n\tI slide across your back\n\tno risk of rushing my affections\n\tI am on board\n\tat your speed\n\nYou!\nYou let the flood of me fill your lungs breathless\n\tyour tumbling heart pounds in my good ear\n\ta song of you rising and falling in languid crescendos\n\t\trocking me gently\n\t\tswallowing me in wet curls\n\t\tsqueezing me from every angle\n\t\ttossing me in slow motion\n\t\televating my body my mind\n\t\tI am pounded on your shore\n\t\tsinking into the sun-warmed sand of your smile\n\t\t(ahhhh, and your surprising splash!)\n\nYou!!!\nI rise to your toll too early, yet\n\tdrowning in gratitude\nAt night we race unicorns until it's too late, and\n\t#sorrynotsorry #nothuman #noregrets\n\nYou…\nRecede to The Source\n\tundulating in other worlds\n\tI dive into your every crest\n\tand we rise rise rise higher\n\tsalt in our crevices\n\tfrothing at the lips\n\nI?\nI ask for a sine\n\ta measure of the mystery\n\tyour crashing whispers\n\trequire no answer\n\tletting the question\n\tanswer itself\n\nAre you my resident frequency?\n\n■",
+            "href": null
+          }
+        ]
+      }
+    }
+  ]
+}
+
+export async function get({ params }) {
+  return {
+    body: {
+      json
+    }
+  }
+}
