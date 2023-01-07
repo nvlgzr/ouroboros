00a9d92e24abf6847bc47f4d367e17bd1905a7d0
Elbong Gearny <elbong@navelgazer.club>
2023-01-07 03:58:24 -0800

    Content: last night and tonight

---
+++ src/routes/StillLife/(content)/coldhandwarmheart/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+  import Error from '$lib/StillLife/Error.svelte';
+  import Cover from '$lib/StillLife/Cover.svelte';
+  import Title from '$lib/StillLife/Title.svelte';
+  import BulletedListItem from '$lib/StillLife/BulletedListItem.svelte';
+  import Callout from '$lib/StillLife/Callout.svelte';
+  import ChildPage from '$lib/StillLife/ChildPage.svelte';
+  import Code from '$lib/StillLife/Code.svelte';
+  import ColumnList from '$lib/StillLife/ColumnList.svelte';
+  import Column from '$lib/StillLife/Column.svelte';
+  import Divider from '$lib/StillLife/Divider.svelte';
+  import Embed from '$lib/StillLife/Embed.svelte';
+  import Header from '$lib/StillLife/Header.svelte';
+  import Image from '$lib/StillLife/Image.svelte';
+  import IndentGroup from '$lib/StillLife/IndentGroup.svelte';
+  import Underline from '$lib/StillLife/Underline.svelte';
+  import InlineCode from '$lib/StillLife/InlineCode.svelte';
+  import InlineColor from '$lib/StillLife/InlineColor.svelte';
+  import Equation from '$lib/StillLife/Equation.svelte';
+  import Link from '$lib/StillLife/Link.svelte';
+  import NumberedListItem from '$lib/StillLife/NumberedListItem.svelte';
+  import Paragraph from '$lib/StillLife/Paragraph.svelte';
+  import Quote from '$lib/StillLife/Quote.svelte';
+  import TodoItem from '$lib/StillLife/TodoItem.svelte';
+  import Toggle from '$lib/StillLife/Toggle.svelte';
+  import { setContext } from 'svelte';
+
+  setContext('pageStuff', {
+    pageId: '9577e24e6e1e4cfabcc90493c3917d3a',
+    title: 'last night and tonight',
+    previous: '',
+    next: ''
+  });
+</script>
+
+<svelte:head>
+  <title>last night and tonight</title>
+</svelte:head>
+
+<Cover coverURL={''} iconEmojiOrURL={'undefined'} />
+
+<Title>last night and tonight</Title>
+
+<Paragraph
+  blockProps={{
+    pageId: '9577e24e6e1e4cfabcc90493c3917d3a',
+    id: 'd4d7fa13-9d23-4cd2-a8ea-481c6be52223',
+    created_time: '2023-01-07T10:57:00.000Z',
+    last_edited_time: '2023-01-07T10:57:00.000Z'
+  }}
+/>
+<Equation
+  block={true}
+  katexString="the\ moon\ almost\ full\\long\ warm\ fingers\ squeeze\ my\ heart\\feeds\ blood\ to\ my\ face"
+/><Paragraph
+  blockProps={{
+    pageId: '9577e24e6e1e4cfabcc90493c3917d3a',
+    id: '2c030f98-f3c1-47c1-b042-533141b6d51e',
+    created_time: '2023-01-07T10:53:00.000Z',
+    last_edited_time: '2023-01-07T10:53:00.000Z'
+  }}
+/>
+<Equation
+  block={true}
+  katexString="your\ voice\ in\ my\ ear\\
+walking\ the\ welcoming\ night\\
+I\ am\ almost\ home"
+/>
---
+++ src/routes/StillLife/(content)/coldhandwarmheart/notion-export.js
@@ -0,0 +1,311 @@
+const json = {
+  "object": "page",
+  "id": "9577e24e-6e1e-4cfa-bcc9-0493c3917d3a",
+  "created_time": "2023-01-07T10:46:00.000Z",
+  "last_edited_time": "2023-01-07T11:56:00.000Z",
+  "created_by": {
+    "object": "user",
+    "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+  },
+  "last_edited_by": {
+    "object": "user",
+    "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+  },
+  "cover": null,
+  "icon": null,
+  "parent": {
+    "type": "database_id",
+    "database_id": "64ff98d9-61c5-4d93-ad3e-b49400276cda"
+  },
+  "archived": false,
+  "properties": {
+    "prevPage": {
+      "id": "BHqW",
+      "type": "rollup",
+      "rollup": {
+        "type": "array",
+        "array": [],
+        "function": "show_original"
+      }
+    },
+    "Public URL": {
+      "id": "CBit",
+      "type": "formula",
+      "formula": {
+        "type": "string",
+        "string": "https://navelgazer.club/StillLife/coldhandwarmheart"
+      }
+    },
+    "§previous": {
+      "id": "C%7DnU",
+      "type": "relation",
+      "relation": [],
+      "has_more": false
+    },
+    "nextPage": {
+      "id": "D%5CYc",
+      "type": "rollup",
+      "rollup": {
+        "type": "array",
+        "array": [],
+        "function": "show_original"
+      }
+    },
+    "Status": {
+      "id": "D%60yI",
+      "type": "select",
+      "select": {
+        "id": "zG{S",
+        "name": "Publishable",
+        "color": "purple"
+      }
+    },
+    "Successful Read-throughs": {
+      "id": "EB%7De",
+      "type": "select",
+      "select": null
+    },
+    "Path": {
+      "id": "E_k%5E",
+      "type": "rich_text",
+      "rich_text": []
+    },
+    "§next": {
+      "id": "QhUz",
+      "type": "relation",
+      "relation": [],
+      "has_more": false
+    },
+    "Next Draft": {
+      "id": "a%3BJF",
+      "type": "relation",
+      "relation": [],
+      "has_more": false
+    },
+    "Slug": {
+      "id": "erwH",
+      "type": "formula",
+      "formula": {
+        "type": "string",
+        "string": "/coldhandwarmheart"
+      }
+    },
+    "§n2sLib": {
+      "id": "gWCh",
+      "type": "rich_text",
+      "rich_text": [
+        {
+          "type": "text",
+          "text": {
+            "content": "$lib/StillLife",
+            "link": null
+          },
+          "annotations": {
+            "bold": false,
+            "italic": false,
+            "strikethrough": false,
+            "underline": false,
+            "code": false,
+            "color": "default"
+          },
+          "plain_text": "$lib/StillLife",
+          "href": null
+        }
+      ]
+    },
+    "Last edited": {
+      "id": "hs%5CU",
+      "type": "last_edited_time",
+      "last_edited_time": "2023-01-07T11:56:00.000Z"
+    },
+    "Created": {
+      "id": "q%3A%7Dm",
+      "type": "created_time",
+      "created_time": "2023-01-07T10:46:00.000Z"
+    },
+    "Previous Draft": {
+      "id": "qdTh",
+      "type": "relation",
+      "relation": [],
+      "has_more": false
+    },
+    "URL": {
+      "id": "r%3Ajk",
+      "type": "url",
+      "url": null
+    },
+    "": {
+      "id": "vDZZ",
+      "type": "created_time",
+      "created_time": "2023-01-07T10:46:00.000Z"
+    },
+    "Mine": {
+      "id": "w%3Dug",
+      "type": "checkbox",
+      "checkbox": true
+    },
+    "Local URL": {
+      "id": "yecv",
+      "type": "formula",
+      "formula": {
+        "type": "string",
+        "string": "http://localhost:5173/StillLife/coldhandwarmheart"
+      }
+    },
+    "Page": {
+      "id": "%7BZlR",
+      "type": "rich_text",
+      "rich_text": [
+        {
+          "type": "text",
+          "text": {
+            "content": "coldhandwarmheart",
+            "link": null
+          },
+          "annotations": {
+            "bold": false,
+            "italic": false,
+            "strikethrough": false,
+            "underline": false,
+            "code": false,
+            "color": "default"
+          },
+          "plain_text": "coldhandwarmheart",
+          "href": null
+        }
+      ]
+    },
+    "Name": {
+      "id": "title",
+      "type": "title",
+      "title": [
+        {
+          "type": "text",
+          "text": {
+            "content": "last night and tonight",
+            "link": null
+          },
+          "annotations": {
+            "bold": false,
+            "italic": false,
+            "strikethrough": false,
+            "underline": false,
+            "code": false,
+            "color": "default"
+          },
+          "plain_text": "last night and tonight",
+          "href": null
+        }
+      ]
+    }
+  },
+  "url": "https://www.notion.so/last-night-and-tonight-9577e24e6e1e4cfabcc90493c3917d3a",
+  "blocks": [
+    {
+      "object": "block",
+      "id": "d4d7fa13-9d23-4cd2-a8ea-481c6be52223",
+      "parent": {
+        "type": "page_id",
+        "page_id": "9577e24e-6e1e-4cfa-bcc9-0493c3917d3a"
+      },
+      "created_time": "2023-01-07T10:57:00.000Z",
+      "last_edited_time": "2023-01-07T10:57:00.000Z",
+      "created_by": {
+        "object": "user",
+        "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+      },
+      "last_edited_by": {
+        "object": "user",
+        "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+      },
+      "has_children": false,
+      "archived": false,
+      "type": "paragraph",
+      "paragraph": {
+        "color": "default",
+        "text": []
+      }
+    },
+    {
+      "object": "block",
+      "id": "39ed3796-d32f-4b4b-b149-9f56c5272391",
+      "parent": {
+        "type": "page_id",
+        "page_id": "9577e24e-6e1e-4cfa-bcc9-0493c3917d3a"
+      },
+      "created_time": "2023-01-07T10:48:00.000Z",
+      "last_edited_time": "2023-01-07T11:47:00.000Z",
+      "created_by": {
+        "object": "user",
+        "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+      },
+      "last_edited_by": {
+        "object": "user",
+        "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+      },
+      "has_children": false,
+      "archived": false,
+      "type": "equation",
+      "equation": {
+        "expression": "the\\ moon\\ almost\\ full\\\\long\\ warm\\ fingers\\ squeeze\\ my\\ heart\\\\feeds\\ blood\\ to\\ my\\ face"
+      }
+    },
+    {
+      "object": "block",
+      "id": "2c030f98-f3c1-47c1-b042-533141b6d51e",
+      "parent": {
+        "type": "page_id",
+        "page_id": "9577e24e-6e1e-4cfa-bcc9-0493c3917d3a"
+      },
+      "created_time": "2023-01-07T10:53:00.000Z",
+      "last_edited_time": "2023-01-07T10:53:00.000Z",
+      "created_by": {
+        "object": "user",
+        "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+      },
+      "last_edited_by": {
+        "object": "user",
+        "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+      },
+      "has_children": false,
+      "archived": false,
+      "type": "paragraph",
+      "paragraph": {
+        "color": "default",
+        "text": []
+      }
+    },
+    {
+      "object": "block",
+      "id": "a5ab8f7d-6474-4286-b2c0-317cab9f2443",
+      "parent": {
+        "type": "page_id",
+        "page_id": "9577e24e-6e1e-4cfa-bcc9-0493c3917d3a"
+      },
+      "created_time": "2023-01-07T10:53:00.000Z",
+      "last_edited_time": "2023-01-07T11:48:00.000Z",
+      "created_by": {
+        "object": "user",
+        "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+      },
+      "last_edited_by": {
+        "object": "user",
+        "id": "9ba3d972-a756-4f33-82db-db0b87d6991d"
+      },
+      "has_children": false,
+      "archived": false,
+      "type": "equation",
+      "equation": {
+        "expression": "your\\ voice\\ in\\ my\\ ear\\\\\nwalking\\ the\\ welcoming\\ night\\\\\nI\\ am\\ almost\\ home"
+      }
+    }
+  ]
+}
+
+export async function get({ params }) {
+  return {
+    body: {
+      json
+    }
+  }
+}
